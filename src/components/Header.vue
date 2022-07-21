3fbb862a7a7341dd82ec389e8607553b72b034cb
vtnboolean <vnthuanx@gmail.com>
2022-07-21 07:25:34 +0200

    add selection

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,18 @@
                     
                     <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-download-logo-30.png" alt="">
                 </a>
+
             </nav>
+
+                <div class="select-box float-right">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Scegli il Genere</option>
+                        <option value="1">Rock</option>
+                        <option value="2">Pop</option>
+                        <option value="3">Jazz</option>
+                        <option value="4">Metal</option>
+                    </select>
+                </div>
         </nav>
     </header>
 </template>
@@ -28,4 +39,10 @@ export default {
             }
         }
     }
+
+    .select-box{
+        margin-right: 50px;
+    }
+
+
 </style>
